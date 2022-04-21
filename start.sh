dbbe9957974535c6b40a1fcdacdf606248e85920
spactllp <102911265+spactllp@users.noreply.github.com>
2022-04-21 15:01:15 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=heroku-app
-/app/tailscale up --advertise-exit-node
 echo Tailscale started
 ALL_PROXY=socks5://localhost:1055/ /app/my-app
